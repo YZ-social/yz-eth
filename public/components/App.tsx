63a6c451a147c559d34f88a83e9ff4be9f4da348
YZ <yz@yz.social>
2025-07-11 23:41:14 -0700

    Fix logo path to use correct base path /yz-eth/yz.png

--- public/components/App.tsx
+++ public/components/App.tsx
@@ -514,7 +514,7 @@ export default function App() {
             }}
           >
             <img
-              src="/yz.png"
+              src="/yz-eth/yz.png"
               alt="YZ Logo"
               style={{
                 width: '120px',
